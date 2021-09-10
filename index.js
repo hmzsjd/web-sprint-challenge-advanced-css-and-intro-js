58f2c2c224b3ba6fa9f00c9d630a1fd697bd688e
Hamza Sajid <github@hamzasajid.com>
2021-09-10 11:03:12 -0700

    task 4 complete

--- index.js
+++ index.js
@@ -240,7 +240,6 @@ function getArtistByIndex(array, idx) {
 
 }
 
-console.log(getArtistByIndex(artists, 8));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -252,10 +251,27 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+
+
+function get20s(array) {
+  let resultArr = [];
+
+
+
+  for (let i=0; i < array.length; i++){
+    
+
+    if ((parseInt(array[i].years.split(" ")[0]) >= 1900) && (parseInt(array[i].years.split(" ")[2]) <= 2000)) {
+      resultArr.push(array[i].name);
+    }
+  }
+  return resultArr;
+
 }
 
+
+console.log("result below");
+console.log(get20s(artists));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
